98d58896de9b7b77a496975ef9ab14a598d8a004
Matthew Shoemaker <shoemaker.277@osu.edu>
2019-12-28 14:45:27 -0500

    environment constants

--- app/config.js
+++ app/config.js
@@ -1,7 +1,22 @@
 (function () {
     "use strict";
     
-    coreLegacy.constant("ENVIRONMENT", "DEV");
-    //coreLegacy.constant("ENVIRONMENT", "PROD");
+    let domain = $location.host();
+    console.log("Current Domain: " + domain);
     
-})(coreLegacy);
+    if (domain.indexOf("localhost") > -1) {
+        coreLegacy.constant("ENVIRONMENT", "DEV");
+        coreLegacy.constant("API_ROOT", "http://localhost:8080/");
+    }
+    else if (domain.indexOf("test") > -1) {
+        coreLegacy.constant("ENVIRONMENT", "TEST");
+        coreLegacy.constant("API_ROOT", "https://test-corelegacy-org-api.herokuapp.com//");
+    }
+    else {
+        coreLegacy.constant("ENVIRONMENT", "PROD");
+        coreLegacy.constant("API_ROOT", "https://corelegacy-org-api.herokuapp.com//");
+    }
+    
+    
+    
+})(coreLegacy, $location);
